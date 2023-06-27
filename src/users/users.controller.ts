dd635e1bf1ceb2d223ea365cfe329553330e85d9
OKORO KELVIN <okorokelvinemoakpooke@yahoo.com>
2023-06-27 05:06:18 +0100

    Login for Single and Coperate

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserDto } from './dtos/UserDto';
+import { LoginDto } from './dtos/LoginDto';
 
 @Controller('users')
 export class UsersController {
@@ -8,10 +9,22 @@ export class UsersController {
         private usersService: UsersService,
       ) {}
 
-    @Post('/signup')
+    @Post('/register')
     async register(@Body() body: UserDto) {
         const user = await this.usersService.register(body.email, body.password,body.name,body.userType);
         return user;
       }
+
+    @Post('/login-single')
+    async loginAsSingle(@Body() body: LoginDto) {
+        const user = await this.usersService.loginAsSingle(body.email, body.password);
+        return user;
+      }
+
+    @Post('/login-corporate')
+      async loginAsCorporate(@Body() body: LoginDto) {
+          const user = await this.usersService.loginAsCorporate(body.email, body.password);
+          return user;
+        }
 
 }
